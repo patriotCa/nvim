b074d008d5dd35f5ae571b7d67a3ce2a1d352f49
patriotCa <kasurman97@gmail.com>
2025-02-11 14:28:59 +0700

    add noice plugin and dependencies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,272 +1,291 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "User FilePost",
-    opts = require "configs.blankline",
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "blankline")
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "User FilePost",
+		opts = require("configs.blankline"),
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "blankline")
 
-      -- local hooks = require "ibl.hooks"
-      -- hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      require("ibl").setup(opts)
+			-- local hooks = require "ibl.hooks"
+			-- hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+			require("ibl").setup(opts)
 
-      dofile(vim.g.base46_cache .. "blankline")
-    end,
-  },
+			dofile(vim.g.base46_cache .. "blankline")
+		end,
+	},
 
-  -- file managing , picker etc
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "antosha417/nvim-lsp-file-operations",
-      config = function()
-        require("lsp-file-operations").setup()
-      end,
-    },
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    opts = function()
-      return require "configs.nvimtree"
-    end,
-  },
+	-- file managing , picker etc
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"antosha417/nvim-lsp-file-operations",
+			config = function()
+				require("lsp-file-operations").setup()
+			end,
+		},
+		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+		opts = function()
+			return require("configs.nvimtree")
+		end,
+	},
 
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show { global = false }
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-      "<c-r>",
-      "<c-w>",
-      '"',
-      "'",
-      "`",
-      "c",
-      "v",
-      "g",
-    },
-    -- keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
-    cmd = "WhichKey",
-    opts = function()
-      return require "configs.whichkey"
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "whichkey")
-      require("which-key").setup(opts)
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+			"<c-r>",
+			"<c-w>",
+			'"',
+			"'",
+			"`",
+			"c",
+			"v",
+			"g",
+		},
+		-- keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+		cmd = "WhichKey",
+		opts = function()
+			return require("configs.whichkey")
+		end,
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "whichkey")
+			require("which-key").setup(opts)
+		end,
+	},
 
-  -- formatting!
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	-- formatting!
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- lsp stuff
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-    opts = function()
-      return require "configs.mason"
-    end,
-  },
+	-- lsp stuff
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+		opts = function()
+			return require("configs.mason")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    event = "User FilePost",
-    config = function()
-      require("configs.lspconfig").defaults()
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		event = "User FilePost",
+		config = function()
+			require("configs.lspconfig").defaults()
+		end,
+	},
 
-  -- load luasnips + cmp related in insert mode only
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require "configs.luasnip"
-        end,
-      },
+	-- load luasnips + cmp related in insert mode only
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				-- snippet plugin
+				"L3MON4D3/LuaSnip",
+				dependencies = "rafamadriz/friendly-snippets",
+				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+				config = function(_, opts)
+					require("luasnip").config.set_config(opts)
+					require("configs.luasnip")
+				end,
+			},
 
-      -- autopairing of (){}[] etc
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
+			-- autopairing of (){}[] etc
+			{
+				"windwp/nvim-autopairs",
+				opts = {
+					fast_wrap = {},
+					disable_filetype = { "TelescopePrompt", "vim" },
+				},
+				config = function(_, opts)
+					require("nvim-autopairs").setup(opts)
 
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-      },
+					-- setup cmp for autopairs
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+				end,
+			},
 
-      -- cmp sources plugins
-      {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-      },
-    },
-    opts = function()
-      return require "configs.cmp"
-    end,
-  },
+			-- cmp sources plugins
+			{
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+			},
+		},
+		opts = function()
+			return require("configs.cmp")
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    cmd = "Telescope",
-    opts = function()
-      return require "configs.telescope"
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		cmd = "Telescope",
+		opts = function()
+			return require("configs.telescope")
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "axelvc/template-string.nvim",
-      "windwp/nvim-ts-autotag",
-    },
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    opts = function()
-      return require "configs.treesitter"
-    end,
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-      require("template-string").setup()
-      require("nvim-ts-autotag").setup {
-        opts = {
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
-          enable_close_on_slash = true, -- Auto close on trailing </
-        },
-      }
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"axelvc/template-string.nvim",
+			"windwp/nvim-ts-autotag",
+		},
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		opts = function()
+			return require("configs.treesitter")
+		end,
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+			require("template-string").setup()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = true, -- Auto close on trailing </
+				},
+			})
+		end,
+	},
 
-  {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    keys = {
-      { "gcc", mode = "n", desc = "Comment toggle current line" },
-      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-      { "gbc", mode = "n", desc = "Comment toggle current block" },
-      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
-    },
-    config = function()
-      -- require("Comment").setup(opts)
-      require("ts_context_commentstring").setup {
-        enable_autocmd = false,
-      }
-      require("Comment").setup {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      }
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    lazy = false,
-    opts = function()
-      return require("configs.comment").todo_comments
-    end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "todo")
-      require("todo-comments").setup(opts)
-    end,
-  },
-  {
-    "mrjones2014/legendary.nvim",
-    lazy = false,
-    config = function()
-      require "configs.legendary"
-    end,
-    dependencies = {
-      "hinell/duplicate.nvim",
-    },
-  },
-  {
-    "stevearc/dressing.nvim",
-    lazy = false,
-    config = function()
-      require("dressing").setup {
-        select = {
-          get_config = function(opts)
-            if opts.kind == "codeaction" then
-              return {
-                backend = "telescope",
-                telescope = require("telescope.themes").get_cursor(),
-              }
-            end
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-  {
-    "mg979/vim-visual-multi",
-    lazy = false,
-  },
-  {
-    "fedepujol/move.nvim",
-    lazy = false,
-    config = function()
-      require("move").setup {
-        line = {
-          enable = true, -- Enables line movement
-          indent = true, -- Toggles indentation
-        },
-        block = {
-          enable = true, -- Enables block movement
-          indent = true, -- Toggles indentation
-        },
-        word = {
-          enable = true, -- Enables word movement
-        },
-        char = {
-          enable = false, -- Enables char movement
-        },
-      }
-    end,
-  },
-  {
-    "j-hui/fidget.nvim",
-    lazy = false,
-    opts = function()
-      return require "configs.fidget"
-    end,
-    config = function(_, opts)
-      require("fidget").setup(opts)
-    end,
-  },
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		keys = {
+			{ "gcc", mode = "n", desc = "Comment toggle current line" },
+			{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+			{ "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+			{ "gbc", mode = "n", desc = "Comment toggle current block" },
+			{ "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+			{ "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+		},
+		config = function()
+			-- require("Comment").setup(opts)
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		opts = function()
+			return require("configs.comment").todo_comments
+		end,
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "todo")
+			require("todo-comments").setup(opts)
+		end,
+	},
+	{
+		"mrjones2014/legendary.nvim",
+		lazy = false,
+		config = function()
+			require("configs.legendary")
+		end,
+		dependencies = {
+			"hinell/duplicate.nvim",
+		},
+	},
+	{
+		"stevearc/dressing.nvim",
+		lazy = false,
+		config = function()
+			require("dressing").setup({
+				select = {
+					get_config = function(opts)
+						if opts.kind == "codeaction" then
+							return {
+								backend = "telescope",
+								telescope = require("telescope.themes").get_cursor(),
+							}
+						end
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
+	{
+		"mg979/vim-visual-multi",
+		lazy = false,
+	},
+	{
+		"fedepujol/move.nvim",
+		lazy = false,
+		config = function()
+			require("move").setup({
+				line = {
+					enable = true, -- Enables line movement
+					indent = true, -- Toggles indentation
+				},
+				block = {
+					enable = true, -- Enables block movement
+					indent = true, -- Toggles indentation
+				},
+				word = {
+					enable = true, -- Enables word movement
+				},
+				char = {
+					enable = false, -- Enables char movement
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		lazy = false,
+		opts = function()
+			return require("configs.fidget")
+		end,
+		config = function(_, opts)
+			require("fidget").setup(opts)
+		end,
+	},
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require("configs.noice")
+		end,
+		config = function(_, opts)
+			require("noice").setup(opts)
+		end,
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 }
